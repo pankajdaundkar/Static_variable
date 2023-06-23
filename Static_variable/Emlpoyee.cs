db5d08320af36cb4db008e487d127df265f60788
pankajdaundkar <phd27993@gmail.com>
2023-06-23 15:11:33 +0530

    Add project files.

---
+++ Static_variable/Emlpoyee.cs
@@ -0,0 +1,102 @@
+﻿using Static_variable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_variable
+{
+    public class Emlpoyee
+    {
+        protected string name;
+        protected int id;
+        protected double bs, hra, ta, da, pf, gs, total;
+        private static int count;
+        public Emlpoyee(string name, int bs)
+        {
+            count++;
+            this.name = name;
+            this.bs = bs;
+            this.id = count;
+        }
+        public static int Getcount()
+        {
+            return count;
+        }
+        public virtual void CalculateSalary()
+
+        {
+            hra = bs * 0.40;
+
+            ta = bs * 0.20;
+
+            da = bs * 0.15;
+
+            pf = bs * 0.12;
+
+            total = (bs + hra + ta + da);
+
+            gs = (total) - pf;
+
+        }
+        public override string ToString()
+        {
+            return $"Name is {name},\nId is {count},\nTotal salary is {total},\nGross salary is {gs}";
+        }
+    }
+    public class Sales_maneger : Emlpoyee
+    {
+        private double cumm;
+        public Sales_maneger(double cumm, int bs, string name) : base(name, bs)
+        {
+            this.cumm = cumm;
+        }
+        public override void CalculateSalary()
+
+        { hra = bs * 0.40;
+
+            ta = bs * 0.20;
+
+            da = bs * 0.15;
+
+            pf = bs * 0.12;
+
+            total = (bs + hra + ta + da + cumm);
+
+            gs = (total) - pf;
+        }
+
+      public override string ToString()
+        {
+            return$"name is {name} \n id is {id}\ntotal salary is {total}\n gross salary is {gs}";
+        }
+    }
+}    public class CEO : Emlpoyee
+{
+    double bonus;
+    public CEO(double bonus, int bs, string name):base(name, bs)
+    {
+        this.bonus = bonus;
+    }
+    public override void CalculateSalary()
+    {
+
+        hra = bs * 0.40;
+
+        ta = bs * 0.20;
+
+        da = bs * 0.15;
+
+        pf = bs * 0.12;
+
+        total = (bs + hra + ta + da + bonus);
+
+        gs = (total) - pf;
+    }
+    public override string ToString()
+    {
+        return $"name is {name},\n id is {id},\ntotal salary is {total},\ngross salary is {gs}";
+    }
+}
---
+++ Static_variable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_variable
+{                                                   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Emlpoyee emp1 = new Emlpoyee("Pankaj", 50000);
+        //    emp1.CalculateSalary();
+        //    Console.WriteLine(emp1.PrintSalary());
+
+        //    Emlpoyee emp2 = new Emlpoyee("Pratik", 100000);
+        //    emp2.CalculateSalary();
+        //    Console.WriteLine(emp2.PrintSalary());
+
+        //    Console.WriteLine(Emlpoyee.Getcount());
+
+           Sales_maneger sales_Maneger = new Sales_maneger(3500,1,"pankaj");
+            sales_Maneger.CalculateSalary();
+            Console.WriteLine(sales_Maneger);
+
+            Emlpoyee emlpoyee = new Emlpoyee("pratik", 50000);
+            emlpoyee.CalculateSalary();
+            Console.WriteLine(emlpoyee);
+
+            CEO ceo = new CEO(5000, 30000, "Sagar");//object create and parameter add
+            ceo.CalculateSalary();//call to void method.object
+            Console.WriteLine(ceo);//print object
+        }
+    }
+}
